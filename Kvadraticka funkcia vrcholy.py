dd82a1e57ac68f70bf0674b07599dd38dd310250
Zero-Cube <82977206+Zero-Cube@users.noreply.github.com>
2021-05-02 19:22:27 +0200

    Update Kvadraticka funkcia vrcholy.py

--- Kvadraticka funkcia vrcholy.py
+++ Kvadraticka funkcia vrcholy.py
@@ -16,29 +16,30 @@ print("____________")
 # f1 = y = ax^2 + bx + c
 # Xv = -b / 2*a
 # Yv = (Xv) * a^2 + b * (Xv) + c     ### Za x dosadime ==> Xv
-
+dva_ = 2 * a
 dva_a = a ** 2
 f1 = f'{a ** 2} - {b} + {c}'
-Xv = (-b / 2 * a)
+Xv = (-b) / dva_
 xvdva = Xv * dva_a
 bxv = Xv * b
+print(f'f1 = y =  {a}ax² + {b}x + {c}')
 print(f'f1 = y =  {a**2}ax² + {b}x + {c}')
 print("________________________")
 def XV():
-    print("Xv = -b / 2 * a"  )
-    print(f"Xv = (-) {(b)} / 2 * {a}")
-    print(f"Xv = (-) {(b)} / {dva_a} ")
+    print("Xv = -b / 2 * a")
+    print(f"Xv = (-{-b}) / 2 * {a}")
+    print(f"Xv = (-{-b}) / {2 * a} ")
     print(f'Xv = {Xv}')
     print("________________________")
 def YX():
-    print(f'Yv = Xv * a² - b * (Xv) + c')
-    print(f"Yv = {dva_a}² {b} * {Xv} + {c}")
-    print(f"Yv= {xvdva} - {- b * Xv} + {+ c}")
-    print("Yv = ",xvdva - -bxv + c)
+    print(f'Yv = (Xv) * a² - (Xv) * b + c')
+    print(f"Yv = ({Xv} * {a}²) - ({Xv} * {b}) + {c}")
+    print(f"Yv= ({xvdva}) - (-{-b * Xv}) + ({+c})")
+    print("Yv = ", xvdva - -bxv + c)
 
 XV(),YX()
 print("=============================")
 xv = -b / 2 * a
 yv = xvdva - -bxv + c
 V = [xv, yv]
-print("V = ",V)
+print("V = ", V)
